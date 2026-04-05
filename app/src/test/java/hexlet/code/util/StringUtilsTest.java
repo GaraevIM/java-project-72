7bd3e645d14c8383b33658baaa9de99aef24fa54
Ильдар <https://github.com/araev/java-project-61>
2026-04-05 21:55:32 +0300

    Fix import order in StringUtilsTest

--- app/src/test/java/hexlet/code/util/StringUtilsTest.java
+++ app/src/test/java/hexlet/code/util/StringUtilsTest.java
@@ -1,8 +1,8 @@
 package hexlet.code.util;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class StringUtilsTest {
 
